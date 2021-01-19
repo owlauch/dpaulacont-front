d330ce8dcc821d0d15005701352305d46e86a3a0
Olivir <olivir.junior@quiver.net.br>
2021-01-19 20:54:30 -0300

    ajuste heroky

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const proxy = require('http-proxy-middleware')
-var apiProxy = proxy('/api', {target: 'https://dpaulacont-back.herokuapp.com'});
+const proxy = require("http-proxy-middleware");
+var apiProxy = proxy.createProxyMiddleware("/api", {
+  target: "https://dpaulacont-back.herokuapp.com",
+});
 
-app.use(apiProxy)
+app.use(apiProxy);
 
 app.use(express.static(__dirname + "/dist"));
 
@@ -14,7 +16,5 @@ app.listen(process.env.PORT || 8080);
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
-
-
 
 console.log("Americas Connectadas!");
